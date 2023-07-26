4bd019d8a1060fce0d3f7f9223733b6b2f32ccaf
TNQT14 <2002quangthai@gmail.com>
2023-07-26 16:47:56 +0700

    Fix null

--- lib/sign_in/sign_in_screen.dart
+++ lib/sign_in/sign_in_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'components/body.dart';
+import '../../size_config.dart';
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(),
       body: Body(),
